4aaa171a22813d0a8080215f0103dde680400cdc
othmanjabes <k11111t12@gmail.com>
2022-08-18 18:19:05 +0300

    update my prject

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () =>{
+const Card = ({name, id, email}) =>{
     return(
-        <div className="bg-light-green">
-            <img alt="photo" src="https://robohash.org/1?size=200x200" />
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="photo" src={`https://robohash.org/${id}?size=200x200`} />
             <div>
-                <h2>Mr. robot</h2>
-                <p>do some thing</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
